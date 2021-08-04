cbea441e6a1875fbf62a14d0c8a0255c3bba3f2a
haminhcong30061999 <hathingocoanh123@gmail.com>
2021-08-04 22:34:43 +0700

    firs

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/nl62hhiBMOM?autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/FW5aNm9cx9A?autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
